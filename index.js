226ed5f9c79d23a60ded784d61b9bca7b875c7a7
smartdev0612 <danieldev0612@gmail.com>
2022-10-28 20:51:14 -0700

    add dockerfiles

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
+app.get('/events', (req, res) => {
+    res.send(events)
+})
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    events.push(event)
+
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: 'OK' });
 })
 
 app.listen(4005, () => {
-    console.log('Listening on 4005');
+    console.log('Event-bus service listening on 4005');
 })
